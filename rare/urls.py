47bbda8ba17dff3d7065b699303817177b9bc114
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-12-14 10:44:04 -0600

    fixed merge conflicts inurls.py and migrated new user model

--- rare/urls.py
+++ rare/urls.py
@@ -19,18 +19,16 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from rareapi.views import PostView
+from rareapi.views import PostView, register_user, check_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'posts', PostView, 'post')
-from rareapi.views import register_user, check_user
 
-router = routers.DefaultRouter(trailing_slash=False)
 urlpatterns = [
     # Requests to http://localhost:8000/register will be routed to the register_user function
     path('register', register_user),
     # Requests to http://localhost:8000/checkuser will be routed to the login_user function
     path('checkuser', check_user),
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('admin/', admin.site.urls)
 ]
